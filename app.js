4199ff9176ca186606fadb70c7817c78ef87067e
TheyKay <murraykyleb@gmail.com>
2020-09-28 22:55:09 -0600

    started writing questions in app js file but decided to move them into inquiries js to keep things clean

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+console.log("Welcome to the Team Generator!")
+
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
